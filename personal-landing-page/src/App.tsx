48eb3a273a0723624a5bb636b48360abcff22ccc
Michael Mei <michaelwmei09@gmail.com>
2026-04-24 19:12:07 -0700

    feature: add vercel speed insights

--- personal-landing-page/src/App.tsx
+++ personal-landing-page/src/App.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -22,6 +23,7 @@ export default function App() {
 
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
